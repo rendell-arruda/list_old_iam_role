571159e5bc52cce4f8bf978fac847b96640b2032
Rendell Arruda <rendellarruda3@gmail.com>
2025-01-19 11:21:35 -0300

    fix: resolve date cutoff

--- list_old_role.py
+++ list_old_role.py
@@ -1,37 +1,24 @@
 import boto3
 from datetime import datetime, timezone, timedelta
-import csv  # Import necessário para trabalhar com arquivos CSV
 
-Account_name = 'sandbox'
-session = boto3.Session(profile_name=Account_name)
-iam_client = session.client('iam')
+#VARIABLES
+#account name 
+account_name = 'sandbox' 
+#qtdd de dias para considerar uma role como inativa.
 days_old = 90
+#list de roles consideradas inativas
 unused_roles = []
 
+session = boto3.Session(profile_name=account_name)
+iam_client = session.client('iam')
 
-
-def list_unused_roles(event, context):
+def lambda_handler(event, context):
     try:
         cutoff_date = datetime.now(timezone.utc) - timedelta(days=days_old)
-        for page in iam_client.get_paginator('list_roles').paginate():
-            for role in page['Roles']:
-                role_name = role['RoleName']
-                print(f'Verificando role :{role_name}')
-                #obtem a ultima data de uso da role
-                last_used_date = iam_client.get_role(RoleName=role_name)['Role'].get('RoleLastUsed',{}).get('LastUsedDate')
-                
-                if not last_used_date:
-                    #role nunca foi usada
-                    print(f"Role '{role_name}' nunca foi usada.\n")
-                    unused_roles.append({'RoleName': role_name, 'LastUsedDate': 'Nunca usada'})
-                elif last_used_date < cutoff_date:
-                    #role está inativa há mais de X dias
-                    print(f"Role '{role_name}' não utilizada desde: {last_used_date}\n")
-                    unused_roles.append({'RoleName': role_name, 'LastUsedDate': last_used_date})
-                    
-                        
+        
+        
     except Exception as e:
         print(f"Erro ao listar roles: {e}")
-    
-    print(unused_roles)           
-if __name__ == '__main__': list_unused_roles({},{})
+ 
+if __name__ == '__main__': 
+    lambda_handler({},{})
